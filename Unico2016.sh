01a4702747dfd5fe6bba71e5456b04c729ffabc0
Elena <ringmeister84@gmail.com>
2016-05-31 23:23:53 +0200

    Nuova directory per esecuzione
    Modificata la directory di esecuzione

--- Unico2016.sh
+++ Unico2016.sh
@@ -5,5 +5,5 @@ mkdir "$HOME/CUR2016"
 cd "$HOME/CUR2016"
 wget "http://jws.agenziaentrate.it/jws/dichiarazioni/2016/CUR16.jnlp"
 fi
-javaws "$HOME/Unico2016/CUR16.jnlp"
+javaws "$HOME/CUR2016/CUR16.jnlp"
 exit
